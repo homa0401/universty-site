9c394cbf7e53803ef136b15e294aff5e164abb4a
homa0401 <humoyun0401@gmail.com>
2022-12-03 22:16:55 +0900

    some changes

--- form-handler.php
+++ form-handler.php
@@ -17,7 +17,7 @@ $to = 'humoyun0401@gmail.com';
 
 $headers = "Form: $email_form  \r\n";
 
-$headers = "Reply-To: $visitor_email \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 
