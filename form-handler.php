004d7ba2f74f67f931d386b3b909934c393806e9
homa0401 <humoyun0401@gmail.com>
2022-12-03 22:51:29 +0900

    php upgrade

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_form = 'info@yourwebsite.com';
+$email_form = 'info@university-sample.netlify.app';
 
 $email_subject = 'New Form Submission';
 
